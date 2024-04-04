9bb301c17e4f890c79edd5db0cc189acc9d5291f
MishterBluesky <christopherlbgraham@live.co.uk>
2024-04-04 12:38:06 +0100

    Update APLbacGoogleColab.sh
    ive added the ability to scale .tsis, as well as cut perfectly.

--- APLbacGoogleColab.sh
+++ APLbacGoogleColab.sh
@@ -6,7 +6,10 @@
 # Define parameters
 start_value=$1
 end_value=$1
-tsi = $2
+tsi=$2
+scale=$3
+max=$(awk "BEGIN {printf \"%.2f\", 705 * $3}")
+min=$(awk "BEGIN {printf \"%.2f\", 315 * $3}")
 echo "APL script initiated with APL value $1.. loading"
 # Loop to create input.str files and carry out subsequent steps
 current_value=$start_value
@@ -27,7 +30,7 @@ current_value=$start_value
 import numpy as np
 import MDAnalysis as mda
 u = mda.Universe('5_Lipidx_$current_value.gro')
-ag = u.select_atoms("byres ((prop abs y < 705) and (prop abs y > 315) and (prop abs z < 705) and (prop abs z > 315)) ")
+ag = u.select_atoms("byres ((prop abs y < $max) and (prop abs y > $min) and (prop abs z < $max) and (prop abs z > $min)) ")
 ag.write('6_Lipidx_$current_value.gro')
 END
 
@@ -36,49 +39,7 @@ END
 
     # Step 7
     gmx editconf -f 6_Lipidx_$current_value.gro -o 7_Lipidx_$current_value.gro -box 40.5 40.5 80 -rotate 0 90 0
-
-    # Step 8
-    ./topollipid.sh 7_Lipidx_$current_value.gro
-    # Step 9
-    gmx grompp -f em.mdp -o emx_$current_value -c 7_Lipidx_$current_value.gro -p topol.top
-
-    # Step 10
-    gmx mdrun -deffnm emx_$current_value -v
-
-    # Step 11
-    gmx solvate -cs ~/water.gro -o 8_Lipidx_$current_value.gro -radius 0.21 -cp emx_$current_value.gro -p topol.top
-
-    # Step 12
-    gmx grompp -f em.mdp -o 9_Lipidx_$current_value -c 8_Lipidx_$current_value.gro
-
-    # Step 13
-    gmx mdrun -deffnm 9_Lipidx_$current_value -v
-
-    # Step 14
-    gmx grompp -f em.mdp -o neutral_$current_value.tpr -p topol.top -c 9_Lipidx_$current_value.gro 
-
-    # Step 15
-    gmx genion -s neutral_$current_value.tpr -o 10_Lipidx_$current_value.pdb -p topol.top -neutral
-
-    # Step 16
-    gmx grompp -f em.mdp -o 11_Lipid20x_$current_value.tpr -p topol.top -c 10_Lipidx_$current_value.pdb
-
-    # Step 17
-    gmx mdrun -deffnm 11_Lipid20x_$current_value -v
-
-    mkdir APL20bac_$current_value
-
-    # Step 19
-    gmx grompp -f mdp_files/cgmdprepx.mdp -o 11b_Lipid20_$current_value.tpr -c 11_Lipid20x_$current_value.gro -p topol.top
-
-    # Step 17
-    gmx mdrun -deffnm 11b_Lipid20_$current_value -v
-
-    # Step 19
-    
-    gmx editconf -f 11b_Lipid20_$current_value -o 11b_$current_value.pdb
-
-
-
-
+echo "max: $max"
+echo "Value of \$3: $3"
+echo "$scale"
 
