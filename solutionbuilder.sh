d2d2b2bf3ead08d68ce2c7a8f178550b1d81d1f8
MishterBluesky <christopherlbgraham@live.co.uk>
2025-03-10 17:00:18 +0000

    Update solutionbuilder.sh

--- solutionbuilder.sh
+++ solutionbuilder.sh
@@ -1,23 +1,16 @@
 
 #!/bin/bash
 
-# Step 1
-
-# Define parameters
-start_value=$1
-name=$2
-protein_pdb=$4
-protein_number=$5
-workdir=$7
-Poredir=$8
-translate=$9
+name=$1
+protein_pdb=$2
+protein_number=$3
+workdir=$4
+Poredir=$5
 scale=${6}
 scale_z=${6}
 cutx=$(awk "BEGIN {printf \"%.2f\", ${6}}")
 cuty=$(awk "BEGIN {printf \"%.2f\", ${6}}")
 cutz=$(awk "BEGIN {printf \"%.2f\", ${6}}")
-./martinishcolab.sh $name $protein_pdb $workdir $Poredir $9
-
 gmx editconf -f 6_$name.gro -o 7_$name.gro -box $cutx $cuty $cutz 
 
     
