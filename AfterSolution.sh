dbf441b24e41dcf367b86e07f4cebcb167d23b27
MishterBluesky <christopherlbgraham@live.co.uk>
2025-04-01 11:56:50 +0100

    Update AfterSolution.sh

--- AfterSolution.sh
+++ AfterSolution.sh
@@ -12,7 +12,7 @@ name=$2
 current_value=$start_value
  
     # Step 9
-    gmx grompp -f em.mdp -o emx_$name -c 7_$name.gro -p topol.top -maxwarn 1000
+    gmx grompp -f em2.mdp -o emx_$name -c 7_$name.gro -p topol.top -maxwarn 1000
 
     # Step 10
     gmx_d mdrun -deffnm emx_$name -v
@@ -21,19 +21,19 @@ current_value=$start_value
     gmx solvate -cs water.gro -o 8_$name.gro -radius 0.21 -cp emx_$name.gro -p topol.top
 
     # Step 12
-    gmx grompp -f em.mdp -o 9_$name -c 8_$name.gro -maxwarn 1000
+    gmx grompp -f em2.mdp -o 9_$name -c 8_$name.gro -maxwarn 1000
 
     # Step 13
     gmx_d mdrun -deffnm 9_$name -v
 
     # Step 14
-    gmx grompp -f em.mdp -o neutral_$name.tpr -p topol.top -c 9_$name.gro -maxwarn 1000
+    gmx grompp -f em2.mdp -o neutral_$name.tpr -p topol.top -c 9_$name.gro -maxwarn 1000
 
     # Step 15
     echo 16 | gmx genion -s neutral_$name.tpr -o 10_$name.pdb -p topol.top -neutral -conc $3
 
     # Step 16
-    gmx grompp -f em.mdp -o 11_$name.tpr -p topol.top -c 10_$name.pdb -maxwarn 1000
+    gmx grompp -f em2.mdp -o 11_$name.tpr -p topol.top -c 10_$name.pdb -maxwarn 1000
 
     # Step 17
     gmx_d mdrun -deffnm 11_$name -v
